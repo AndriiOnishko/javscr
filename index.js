f50fdee97da2f5e61b43c82a5f09c93d20dcb33d
Andrey <or4ini@gmail.com>
2016-07-05 13:11:02 +0000

    fib

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var x=prompt('incert number', '');
 
-function fibo(x)
+function fib(x)
 {   if (x<3)
         return 1;
     else
-        return fibo(x-1) + fibo(x-2);
+        return fib(x-1) + fib(x-2);
 }
 
 for (var i = 1; i <= x; i++)
-    alert(fibo(i));
+    alert(fib(i));
